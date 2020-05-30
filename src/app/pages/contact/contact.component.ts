d6097af93e489440100641ad5a5845517adec2dd
Brendan Weirtz <brendanweirtz@gmail.com>
2020-05-29 23:58:18 -0400

    Contact server init

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-contact',
@@ -9,28 +10,55 @@ import { Title } from '@angular/platform-browser';
 })
 export class ContactComponent implements OnInit {
 
-  FormData: FormGroup;
+  firstName: string;
+  lastName: string;
+  email: string;
+  message: string;
+  contactForm;
 
-  constructor(private builder: FormBuilder,
-              private title: Title) { }
+  constructor(private title: Title, public http: HttpService,formBuilder: FormBuilder) {
+    this.contactForm = formBuilder.group({
+      firstNameInput: new FormControl('', Validators.compose([Validators.required, Validators.minLength(6)])),
+      lastNameInput: new FormControl('', Validators.compose([Validators.required, Validators.minLength(6)])),
+      emailInput: new FormControl('', Validators.compose([Validators.required, Validators.email])),
+      messageInput: new FormControl('', Validators.compose([Validators.required]))
+    });
+  }
 
   //AWS - SMTP Server.
   //Nodemailer - email
   //Send user form to Nodemailer, who passes to AWS Secure Email Service.
-  submitForm(){
+  submit(){
 
+    let emailContent = {
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email,
+      message: this.message
+    }
+
+    this.http.sendEmail("http://localhost:3000/sendmail", emailContent).subscribe(
+      data => {
+        let res:any = data; 
+        console.log(
+          `Message id is ${res.messageId}`
+        );
+      },
+      err => {
+        console.log(err);
+      },() => {
+        console.log("Success")
+      }
+    );
+  //end submit
   }
 
   ngOnInit() {
     window.scroll(0,0);
     //Set title
     this.title.setTitle("Contact");
-  
-    this.FormData = this.builder.group({
-      Fullname: new FormControl('', [Validators.required]),
-      Email: new FormControl('', [Validators.compose([Validators.required, Validators.email])]),
-      Comment: new FormControl('', [Validators.required])
-      })
   }
+
+
 
 }
