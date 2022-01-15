29f85688165ee52008d0e62de6f8aa13d2da1771
Brendan Weirtz <brendanweirtz@gmail.com>
2022-01-15 17:16:28 -0500

    Home refresh

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,13 +1,9 @@
 import { Component, OnInit, AfterViewInit, PLATFORM_ID, Inject } from '@angular/core';
-// import * as $ from 'jquery';
-// import 'jqueryui'
 import { Title } from '@angular/platform-browser';
 import { isPlatformBrowser } from '@angular/common';
-// import * as THREE from 'three';
-// import { GLTFLoader } from './node_modules/three/jsm/loader/GLTFLoader.js';
-// import { AsciiEffect } from './node_modules/examples/jsm/effects/AsciiEffect.js';
-// import { TrackballControls } from './examples/jsm/controls/TrackballControls.js';
-
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { AsciiEffect } from 'three/examples/jsm/effects/AsciiEffect.js';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -29,17 +25,86 @@ export class AboutComponent implements OnInit, AfterViewInit {
     
     //Set title
     this.title.setTitle("Brendan Weirtz");
+  
+  
+  
+    //THREE JS
+    let camera, controls, scene, renderer, effect;
+    let model;
+    const start = Date.now();
+    let container = document.getElementById( 'three-main' );
+    let dimensions = container.getBoundingClientRect();
+
+    init();
+    animate();
+
+    function init() {
+
+ 
+
+
+
+
+        camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 2000 );
+        camera.position.y = 0;
+        camera.position.z = 4;
+        camera.position.x = 0.6;
+
+        scene = new THREE.Scene();
+
+        const pointLight1 = new THREE.PointLight( 0xffffff, 42.4  );
+        pointLight1.position.set( 20, 10, -10 );
+        scene.add( pointLight1 );
+
+        const pointLight2 = new THREE.PointLight( 0xffffff, 4.4 );
+        pointLight2.position.set( -2, 10, 10 );
+        scene.add( pointLight2 );
+
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize( dimensions.width, dimensions.height );
+        
+        effect = new AsciiEffect( renderer, ' "', { invert: true } );
+        effect.setSize( dimensions.width, dimensions.height );
+        effect.domElement.style.color = 'lightgrey';
+
+        container.appendChild( effect.domElement );
+
+        window.addEventListener( 'resize', onWindowResize );
+
+        const loader = new GLTFLoader();
+
+        loader.load( '/assets/models/about/weirtz3D.glb', function ( gltf ) {
+        model = gltf.scene;
+        model.scale.set(1,0.6,1);
+        scene.add( model );
+
+        }, undefined, function ( error ) {
+
+        console.error( error );
+
+        } );
+
+    }
+
+    function onWindowResize() {
+        // camera.aspect = dimensions.width /dimensions.height;
+        camera.updateProjectionMatrix();
+        renderer.setSize( dimensions.width, dimensions.height );
+        effect.setSize( dimensions.width, dimensions.height );
+    }
+
+
+    function animate() {
+        requestAnimationFrame( animate );
+        render();
+    }
+
+    function render() {
+        const timer = Date.now() - start;
+
+        if(model) model.rotation.y = timer * -0.001;
+        effect.render( scene, camera );
+    }
+    //END THREE JS
   }
-
-
-
-
-
-
-
-
-
-
-
-
 }
